5b96cf07f2fc3589464d8365ec4729d754f6a57e
Kalyani Kale <kalyanikale192@gmail.com>
2026-03-29 16:03:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,17 +18,25 @@ model = pickle.load(open('model.pkl', 'rb'))
 st.title("Credit Card Fraud Detection")
 
 amount = st.number_input("Transaction Amount", min_value=0.0)
+option = st.selectbox(
+    "Choose transaction type for demo:",
+    ["Normal Transaction", "Fraud Transaction"]
+)
 
-if st.button("Check Fraud"):
+    normal_sample = [10,0.38,0.61,-0.87,-0.09,2.92,3.31,0.47,0.53,-0.55,0.30,
+-0.25,-0.32,-0.09,0.36,0.92,-0.12,-0.80,0.35,0.70,0.12,0.04,0.23,
+0.009,0.99,-0.76,-0.49,0.04,-0.05,50]
 
-    sample = [10,0.384978215,0.616109459,-0.874299703,-0.094018626,2.924584378,
-    3.317027168,0.470454672,0.538247228,-0.558894612,0.309755394,-0.259115564,
-    -0.326143234,-0.090046723,0.362832369,0.928903661,-0.129486811,-0.809978926,
-    0.35998539,0.707663826,0.125991576,0.049923686,0.238421512,0.009129869,
-    0.99671021,-0.767314827,-0.492208295,0.042472442,-0.054337388,9.99]
+    fraud_sample = [-1.35,-0.07,2.53,1.37,-0.33,0.46,0.23,0.09,-0.55,0.08,
+-0.25,-0.11,0.22,-0.16,0.07,0.10,-0.02,0.01,0.50,-0.02,-0.01,0.02,
+0.05,0.10,-0.20,0.12,-0.01,0.03,-0.02,100]
 
-    # update amount
-    sample[-1] = amount
+  if st.button("Check Fraud"):
+
+    if option == "Normal Transaction":
+        sample = normal_sample
+    else:
+        sample = fraud_sample
 
     prediction = model.predict([sample])
     prob = model.predict_proba([sample])[0][1]
@@ -36,4 +44,4 @@ if st.button("Check Fraud"):
     if prediction[0] == 1:
         st.error(f"Fraud Detected! Probability: {prob:.2f}")
     else:
-        st.success(f"Normal Transaction. Probability: {prob:.2f}")
+        st.success(f" Normal Transaction. Probability: {prob:.2f}")
