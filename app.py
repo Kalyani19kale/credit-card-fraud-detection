f543d83bcf843f9ff2e5cc308221f261ed855c36
Kalyani Kale <kalyanikale192@gmail.com>
2026-03-29 22:55:27 +0530

    Update model file name from 'fraud_model.pkl' to 'model.pkl'

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import numpy as np
 import pickle
 
 # Load trained model and scaler
-with open("fraud_model.pkl", "rb") as f:
+with open("model.pkl", "rb") as f:
     model = pickle.load(f)
 with open("scaler.pkl", "rb") as f:
     scaler = pickle.load(f)
